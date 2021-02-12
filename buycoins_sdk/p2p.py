d0d1cf184626729fe597032185b337547c86e8c6
Bashorun97 <bashorun.emma@gmail.com>
2021-02-12 01:27:07 +0100

    p2p restructure

--- buycoins_sdk/p2p.py
+++ buycoins_sdk/p2p.py
@@ -1,13 +1,19 @@
 from gcore.mutations import Buy, PostLimitOrder, PostMarketOrder
-from gcore.queries import GetOrders, GetMarketBook, GetDynamicPriceExpiry
+from gcore.queries import GetOrders, GetMarketBook, GetDynamicPriceExpiry, BuyCoinsPrices
 from typing import List, Optional
 
 
 # P2P purchase
-class BuycoinsP2P():
-    def __init__(self):
-        super().__init__()
-    
+class BuyCoinsP2P():    
+    # This query type doesn't exist on BuyCoins server but it's specified in the documentation
+    # def get_buycoins_prices(self, side: str, mode: str, cryptocurrency: str, subfields: List) -> str:
+    #    return BuyCoinsPrices().queryObject(
+    #        side=side,
+    #        mode=mode,
+    #        cryptocurrency=cryptocurrency,
+    #        subfields=subfields
+    #    )
+
     def get_dynamic_price(self, status: str) -> str:
         return GetDynamicPriceExpiry(
             status=status
@@ -15,31 +21,32 @@ class BuycoinsP2P():
 
     def get_orders(self, status: str, subfields: List) -> str:
         return GetOrders(
-            status=status
-        ).queryObject(subfields=subfields)
+            status=status,
+            subfields=subfields
+        ).queryObject()
 
-    def get_market_book(self, status: str, subfields: List) -> str:
+    def get_market_book(self, subfields: List) -> str:
         return GetMarketBook(
-            status=status
-        ).queryObject(subfields=subfields)
+            subfields=subfields
+        ).queryObject()
 
-    def limit_order(self, subfields: List, order_side: str, coin_amount: float, cryptocurrency, price_type: str, price_type_value: Optional[List[tuple]]=None):
+    def post_limit_order(self, order_side: str, coin_amount: float, cryptocurrency: str, price_type: str, subfields: List, static_price=None, dynamic_exchange_rate=None):
         order = PostLimitOrder(
-            subfields=subfields,
             order_side=order_side,
             coin_amount=coin_amount,
             cryptocurrency=cryptocurrency,
             price_type=price_type,
-            price_type_value=price_type_value
+            static_price=static_price,
+            dynamic_exchange_rate=dynamic_exchange_rate,
+            subfields=subfields
         )
         return order.Mutate()
 
-    def market_order(self, subfields: List, order_side: str, coin_amount: float, cryptocurrency: str):
+    def post_market_order(self, order_side: str, coin_amount: float, cryptocurrency: str, subfields: List):
         order = PostMarketOrder(
-            subfields=subfields,
             order_side=order_side,
             coin_amount=coin_amount,
-            cryptocurrency=cryptocurrency
+            cryptocurrency=cryptocurrency,
+            subfields=subfields
         )
         return order.Mutate()
-        
